5a54b7f437d76e24cc9b185c67e6f5c072485ef7
fultzmealready <92765547+fultzmealready@users.noreply.github.com>
2025-08-29 13:36:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -292,11 +292,6 @@ with st.form("trade_submit_form", clear_on_submit=False):
         st.success("Trade added to Google Sheet.")
         st.rerun()
 
-    if del_btn and to_delete_ids:
-        new_df = delete_by_ids(to_delete_ids)
-        st.success(f"Deleted {len(to_delete_ids)} trade(s).")
-        st.rerun()
-
 # ==== Load from Google Sheets ====
 df = load_trades()
 
